548f65ec8b1f24a766ca6267337d34b989302121
MSI\KAI <nhanksd85@gmail.com>
2023-04-10 12:13:37 +0700

    Init the tempate

---
+++ PrivateTasks/private_task_1.py
@@ -0,0 +1,6 @@
+class Task1:
+    def __int__(self):
+        return
+
+    def Task1_Run(self):
+        print("Task 1 is activated!!!!")
---
+++ PrivateTasks/private_task_2.py
@@ -0,0 +1,6 @@
+class Task2:
+    def __int__(self):
+        return
+
+    def Task2_Run(self):
+        print("Task 2 is activated!!!!")
